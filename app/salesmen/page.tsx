cd2b77b0c8afea7fc7266d62c920213176290bf2
SurajKharkwal <kharkwalsuraj13@gmai.com>
2024-03-28 00:16:27 +0530

    Bug fixing

--- app/salesmen/page.tsx
+++ app/salesmen/page.tsx
@@ -1,13 +1,14 @@
-"use client"
-import Scanner from '@/components/salesmen/Scanner'
-import React from 'react'
+"use client";
+import Scanner from "@/components/salesmen/Scanner";
+import React from "react";
 
 const page = () => {
   return (
     <div>
       {/* <Scanner /> */}
+      hello
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
